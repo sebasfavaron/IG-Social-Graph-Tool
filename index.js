82e2592fbbc737a6bdf76da01c6b3783efe4bbdc
Sebastian Favaron <sebastianfavaron@gmail.com>
2023-04-03 19:11:15 -0300

    Better highlighted how clicked node and its followers look
    + eliminated magic values (parametrized all styles)

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ export function drawGraph({ nodes, links }) {
   const nodeCircleSize = 2;
   const textSize = 12;
   const linksStrokeWidth = 1;
-  const circlesStrokeWidth = 0.5;
+  const circlesStrokeWidth = 1;
   const textsDx = 10;
   const textsDy = 0.35;
-  const selectedNodeColor = '#fc7a2f';
+  const mainColor = 'black';
+  const secondaryColor = 'white';
+  const linkOpacity = 0.04;
+  const linkColor = '#868585';
 
   // Set up the SVG element and add the nodes and links
   const svg = d3.select('svg'),
@@ -24,7 +27,8 @@ export function drawGraph({ nodes, links }) {
     .data(links)
     .enter()
     .append('line')
-    .attr('class', 'link');
+    .attr('class', 'link')
+    .attr('id', (d) => convertToValidIdFormat(`link-${d.source}-${d.target}`));
 
   const node = container
     .selectAll('.node')
@@ -44,8 +48,8 @@ export function drawGraph({ nodes, links }) {
   const circles = node
     .append('circle')
     .attr('r', nodeCircleSize)
-    .attr('fill', 'black')
-    .attr('stroke', 'white')
+    .attr('fill', mainColor)
+    .attr('stroke', secondaryColor)
     .attr('stroke-width', circlesStrokeWidth);
 
   const texts = node
@@ -54,28 +58,43 @@ export function drawGraph({ nodes, links }) {
     .attr('dx', textsDx)
     .attr('dy', `${textsDy}em`)
     .attr('font-size', `${textSize}px`)
+    .style('fill', mainColor)
     .attr('font-family', 'Arial');
 
   const stylesClass = () => {
     let k = 1;
     let clickedNode = clickedNodeClass();
+    const resetStyles = () => {
+      clickedNode.setNode(null);
+      node.style('opacity', 1);
+      node
+        .select('text')
+        .attr('font-size', textSize / k)
+        .style('fill', mainColor)
+        .style('text-shadow', 'none');
+      node
+        .select('circle')
+        .attr('fill', mainColor)
+        .attr('stroke', secondaryColor)
+        .attr('r', nodeCircleSize / k);
+
+      const links = document.querySelectorAll('[id^="link"]');
+      links.forEach((link) => {
+        link.style.strokeOpacity = linkOpacity;
+        link.style.stroke = linkColor;
+      });
+    };
+
     return {
       setK: (newK) => {
         k = newK;
         clickedNode.setK(newK);
       },
       clickedNode,
-      resetStyles: () => {
-        node.style('opacity', 1);
-        node.select('text').attr('font-size', textSize / k);
-        node.select('circle').attr('fill', 'black');
-        node.select('circle').attr('r', nodeCircleSize / k);
-      },
+      resetStyles,
       setBackgroundNodesStyles: () => {
+        resetStyles();
         node.style('opacity', 0.1);
-        node.select('text').attr('font-size', textSize / k);
-        node.select('circle').attr('fill', 'black');
-        node.select('circle').attr('r', nodeCircleSize / k);
       },
       setStyles: () => {
         if (k > 1) {
@@ -94,6 +113,12 @@ export function drawGraph({ nodes, links }) {
   const clickedNodeClass = () => {
     let node = null;
     let k = 1;
+    const color = secondaryColor;
+    const bgColor = mainColor;
+
+    // Old pulp fiction look
+    // const color = 'gold';
+    // .style('text-shadow', '2px 2px 0px #FF0000');
     return {
       setK: (newK) => {
         k = newK;
@@ -102,21 +127,29 @@ export function drawGraph({ nodes, links }) {
         node = newNode;
       },
       setStyles: () => {
-        console.log({ node });
         if (!node) {
           return;
         }
 
         node.style('opacity', 1);
-        node.select('text').attr('font-size', (textSize * 2) / k);
+        node
+          .select('text')
+          .attr('font-size', (textSize * 2) / k)
+          .style('fill', color)
+          .style(
+            'text-shadow',
+            `1px 1px 0 ${bgColor}, 1px -1px 0 ${bgColor}, -1px 1px 0 ${bgColor}, -1px -1px 0 ${bgColor}, 1px 0px 0 ${bgColor}, 0px 1px 0 ${bgColor}, -1px 0px 0 ${bgColor}, 0px -1px 0 ${bgColor}`
+          );
         node
           .select('circle')
-          .attr('fill', selectedNodeColor)
+          .attr('fill', color)
+          .attr('stroke', bgColor)
           .attr('r', (nodeCircleSize * 2) / k);
       },
     };
   };
   const styleManager = stylesClass();
+  styleManager.resetStyles();
 
   // Add zoom behavior to the SVG element
   const zoom = d3
@@ -147,13 +180,23 @@ export function drawGraph({ nodes, links }) {
     styleManager.clickedNode.setNode(d3.select(this));
     styleManager.clickedNode.setStyles();
 
-    // Highlight the clicked node's followers
+    // Highlight the clicked node's followers and their links
     const usersFollowers = getUsersFollowers(event.id);
-    usersFollowers.forEach((follower) =>
-      d3
-        .select(`#${convertToValidIdFormat(follower.source.id)}`)
-        .style('opacity', 1)
-    );
+    usersFollowers.forEach((follower) => {
+      const followerNode = d3.select(
+        `#${convertToValidIdFormat(follower.source.id)}`
+      );
+      followerNode.style('opacity', 0.8);
+
+      const followerLink = document.querySelector(
+        '#' +
+          convertToValidIdFormat(
+            `link-${follower.source.id}-${follower.target.id}`
+          )
+      );
+      followerLink.style.strokeOpacity = 0.8;
+      followerLink.style.stroke = 'red';
+    });
   });
   d3.select('body').on('click', () => {
     styleManager.resetStyles();
